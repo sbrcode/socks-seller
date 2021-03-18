5246b18b8da522a9b9024ea3f416ff38b94a8f94
SylvainBR <3095440-Sylvainn7@users.noreply.gitlab.com>
2021-03-18 15:31:15 +0100

    app-architecture

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,16 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <SocksSeller/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import SocksSeller from '@/components/SocksSeller.vue'
 
 export default {
-  name: 'Home',
   components: {
-    HelloWorld
+    SocksSeller
   }
 }
 </script>
